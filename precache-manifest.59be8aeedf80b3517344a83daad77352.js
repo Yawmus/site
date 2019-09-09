5fcf97748ad7559e795f5ea2191974e247f5f212
yawmus <peterj@USA-IT-CXG8WM.corp.unicity.net>
2019-09-09 16:12:47 -0600

    Updates

---
+++ precache-manifest.59be8aeedf80b3517344a83daad77352.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30055cd8c243c03b24095849f91ebf80",
+    "url": "yawmus.github.io/index.html"
+  },
+  {
+    "revision": "0ce4a0daca4ecc3b5f6c",
+    "url": "yawmus.github.io/static/css/main.41fc0eed.chunk.css"
+  },
+  {
+    "revision": "1c14fe117e3606636b15",
+    "url": "yawmus.github.io/static/js/2.2f55e4a6.chunk.js"
+  },
+  {
+    "revision": "0ce4a0daca4ecc3b5f6c",
+    "url": "yawmus.github.io/static/js/main.b19e5160.chunk.js"
+  },
+  {
+    "revision": "8d262f97860c81c7dfe0",
+    "url": "yawmus.github.io/static/js/runtime~main.b3626aad.js"
+  },
+  {
+    "revision": "9383f4b0bc1d264a5a7e094eb1ed0c0b",
+    "url": "yawmus.github.io/static/media/UbuntuMono-R.9383f4b0.ttf"
+  },
+  {
+    "revision": "c9b6fece5ff1836fcef6be1f605b19a3",
+    "url": "yawmus.github.io/static/media/animal.c9b6fece.jpg"
+  },
+  {
+    "revision": "2f83488eab7ac5205dfd909d3664cbdb",
+    "url": "yawmus.github.io/static/media/background.2f83488e.jpg"
+  },
+  {
+    "revision": "2ac886e9624fbadf683b355441c5019b",
+    "url": "yawmus.github.io/static/media/border.2ac886e9.png"
+  },
+  {
+    "revision": "f2b1bab80e72cb860d2b5f4792f7e614",
+    "url": "yawmus.github.io/static/media/fog.f2b1bab8.jpg"
+  },
+  {
+    "revision": "cfdfb87a8978c8aa931112b20173b352",
+    "url": "yawmus.github.io/static/media/fog2.cfdfb87a.jpg"
+  },
+  {
+    "revision": "10cf8f2c56ec40f10aacb4110ad535fe",
+    "url": "yawmus.github.io/static/media/fog3.10cf8f2c.jpg"
+  },
+  {
+    "revision": "79441c4a11e3180386a4559f53cd6f95",
+    "url": "yawmus.github.io/static/media/fog4.79441c4a.jpg"
+  },
+  {
+    "revision": "5b21d812ce4728d059d5ce47f5465cc9",
+    "url": "yawmus.github.io/static/media/fog5.5b21d812.jpg"
+  },
+  {
+    "revision": "04e41131c49ad0683c675244617e46dd",
+    "url": "yawmus.github.io/static/media/fog6.04e41131.jpg"
+  },
+  {
+    "revision": "02189f10e6a15fe526c13ca9e2d7d317",
+    "url": "yawmus.github.io/static/media/hapi.02189f10.png"
+  },
+  {
+    "revision": "3361f50dd32710bfcb0063ea26fc920b",
+    "url": "yawmus.github.io/static/media/loader.3361f50d.gif"
+  },
+  {
+    "revision": "eb8a1616bb885fac6b5643131b24c037",
+    "url": "yawmus.github.io/static/media/millipede.eb8a1616.png"
+  },
+  {
+    "revision": "429481559627cc7e62f072e59e72ac74",
+    "url": "yawmus.github.io/static/media/pragma.42948155.png"
+  },
+  {
+    "revision": "3beb420113216ca618a10d27d91531da",
+    "url": "yawmus.github.io/static/media/raz1.3beb4201.png"
+  },
+  {
+    "revision": "8495e86712867592ec85caf99909992c",
+    "url": "yawmus.github.io/static/media/raz2.8495e867.png"
+  },
+  {
+    "revision": "d245591de9fd5d31bde58560ca237b03",
+    "url": "yawmus.github.io/static/media/raz3.d245591d.png"
+  },
+  {
+    "revision": "e7adef03f357b95f841f8f33527985dc",
+    "url": "yawmus.github.io/static/media/raz4.e7adef03.png"
+  },
+  {
+    "revision": "dd5e392a1a808d9c0140290fd6ea1b2a",
+    "url": "yawmus.github.io/static/media/raz5.dd5e392a.png"
+  },
+  {
+    "revision": "7f9feb808614dddacdc0ffc8bdeb2bef",
+    "url": "yawmus.github.io/static/media/raz6.7f9feb80.png"
+  },
+  {
+    "revision": "dc30417037aaf5a9b53d118cf475b362",
+    "url": "yawmus.github.io/static/media/redux.dc304170.webp"
+  },
+  {
+    "revision": "a7c60bc0fac29cf72340c21880965570",
+    "url": "yawmus.github.io/static/media/roguelike2.a7c60bc0.png"
+  },
+  {
+    "revision": "4014dfe7ab53befb43bed5c5ace4039b",
+    "url": "yawmus.github.io/static/media/server4.4014dfe7.png"
+  },
+  {
+    "revision": "befb0ac204b0f6c61a6b03b1f19f7696",
+    "url": "yawmus.github.io/static/media/server5.befb0ac2.png"
+  },
+  {
+    "revision": "9151cb14d75d2577935ac5ed51518ef7",
+    "url": "yawmus.github.io/static/media/the1.9151cb14.png"
+  },
+  {
+    "revision": "c10957b11a2983ee26cb94bffd8c9e0c",
+    "url": "yawmus.github.io/static/media/the2.c10957b1.png"
+  },
+  {
+    "revision": "61bcca3a71a69acf1cbad0d64b266032",
+    "url": "yawmus.github.io/static/media/the3.61bcca3a.png"
+  },
+  {
+    "revision": "08676e0bd67554d712d8210e184bc5cd",
+    "url": "yawmus.github.io/static/media/the4.08676e0b.png"
+  },
+  {
+    "revision": "e4035ff4c7eca5ff69825034ac194d85",
+    "url": "yawmus.github.io/static/media/the5.e4035ff4.png"
+  },
+  {
+    "revision": "68e4bf699d2bab02acd1377f5f1526d2",
+    "url": "yawmus.github.io/static/media/the6.68e4bf69.png"
+  },
+  {
+    "revision": "0d1041e26ae8e98b4040cec19d7d6087",
+    "url": "yawmus.github.io/static/media/the7.0d1041e2.png"
+  },
+  {
+    "revision": "b40b5f705caf6041155068b3cee667b7",
+    "url": "yawmus.github.io/static/media/throughhereyes.b40b5f70.png"
+  }
+]);
