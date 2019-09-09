ce65a20b8bb65e4d42100288177edc95af9a9851
yawmus <peterj@USA-IT-CXG8WM.corp.unicity.net>
2019-09-09 17:31:52 -0600

    Updates

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM node:latest
-#RUN mkdir -p backend/src/
-
-RUN npm install
-
-
-WORKDIR /app
-EXPOSE 4000
-#RUN npm run dev
--- src/containers/return.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Redirect } from 'react-router-dom'
-
-//const GoHome = () => <h1>Not found</h1>
-const RedirectTo = (path) => {
-  return <Redirect to={`${path}`} />
-}
-export default RedirectTo
